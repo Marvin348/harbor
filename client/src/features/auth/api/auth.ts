3701dccc27c9d7c5cdd9fe93b8b9374760ee9bc9
Marvin <marvinITprogram@gmail.com>
2026-09-02 13:02:18 +0200

    feat: openapi-types

--- client/src/features/auth/api/auth.ts
+++ client/src/features/auth/api/auth.ts
@@ -1,13 +1,17 @@
 import { apiClient } from "@/lib/apiClient.ts";
 import type { RegisterFields } from "@/features/auth/schema/registerSchema.ts";
 import type { LoginFields } from "@/features/auth/schema/loginSchema.ts";
+import type { RegisterResponse } from "@/api/generated/models/register-response.ts";
+import type { LoginResponse } from "@/api/generated/models/login-response.ts";
 
-export const register = async (data: RegisterFields) => {
+export const register = async (
+  data: RegisterFields,
+): Promise<RegisterResponse> => {
   const res = await apiClient.post("auth/register", data);
   return res.data;
 };
 
-export const login = async (data: LoginFields) => {
+export const login = async (data: LoginFields): Promise<LoginResponse> => {
   const res = await apiClient.post("auth/login", data);
   return res.data;
 };
